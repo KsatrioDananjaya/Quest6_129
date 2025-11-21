120f890fdf845dd6aa7981d2158abed78953d1b7
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2025-11-21 15:28:22 +0700

    mengedit formulir dengan menambahkan txt nama,alamat, dan gender yang dapat di simpan

--- app/src/main/java/com/example/p8_mvvm/view/Formulir.kt
+++ app/src/main/java/com/example/p8_mvvm/view/Formulir.kt
@@ -18,6 +18,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -33,6 +37,10 @@ fun FormIsian(
     OnSubmitBtnClick : () -> Unit,
     modifier: Modifier = Modifier
 ){
+    var txtNama by rememberSaveable { mutableStateOf("") }
+    var txtAlamat by remember { mutableStateOf("") }
+    var txtGender by remember {mutableStateOf("")}
+    val listData: MutableList<String> = mutableListOf(txtNama,txtGender,txtAlamat)
     Scaffold(modifier = Modifier,
         {
             TopAppBar(
