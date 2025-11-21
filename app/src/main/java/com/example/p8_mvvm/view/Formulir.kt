0c81d5829621b44d14262460cdc76f8905a13206
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2025-11-21 15:33:42 +0700

    menambahkan txtgender dan juga pilihanjk agar dapat mengingat kelamin

--- app/src/main/java/com/example/p8_mvvm/view/Formulir.kt
+++ app/src/main/java/com/example/p8_mvvm/view/Formulir.kt
@@ -69,12 +69,14 @@ fun FormIsian(
                 .padding(20.dp)
                 .width(250.dp), thickness = Thickness, color = Color.Red)
             Row {
-                jenisK.forEach {
+                pilihanJK.forEach {
                         item ->
                     Row(verticalAlignment = Alignment.CenterVertically) {
                         RadioButton(
-                            selected = false,
-                            onClick = {item}
+                            selected = txtGender == item,
+                            onClick = {
+                                txtGender = item
+                            }
                         )
                         Text(text = item)
                     }
