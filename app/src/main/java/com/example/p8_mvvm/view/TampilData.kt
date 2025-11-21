9b668eae71aa816c54bd72fb90a81e840a9686cd
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2025-11-21 15:50:17 +0700

    lupa mengedit onclick button

--- app/src/main/java/com/example/p8_mvvm/view/TampilData.kt
+++ app/src/main/java/com/example/p8_mvvm/view/TampilData.kt
@@ -62,7 +62,7 @@ fun TampilData(
                 Spacer(modifier = Modifier.height(10.dp))
                 Button(
                     modifier = Modifier.fillMaxWidth(),
-                    onClick = onBackBtnClick
+                    onClick = onBackBtnClicked
                 ) {
                     Text(text = stringResource(id = R.string.back))
                 }
